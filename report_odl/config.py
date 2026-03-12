93aa734b7fab3ecf9975fd53a994ceaaba541e25
Progettomich <progetto.humanitasits@gmail.com>
2026-03-12 12:34:43 +0100

    Update API_BASE URL for report_odl configuration

--- report_odl/config.py
+++ report_odl/config.py
@@ -1,7 +1,7 @@
 # report_odl/config.py
 
 # --- Configurazione API ---
-API_BASE = "http://10.38.169.149:3500/api/v1/zMaintenance/rdi"
+API_BASE = "http://10.38.169.149:3500/api/v1/zMaintenance/report/odl"
 API_USER = ***********
 API_PASS = ***********
 DATE_FROM = "2026-01-01"
