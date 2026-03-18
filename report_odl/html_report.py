0a0189ee01acb69adee69a50709f1e5c045bc1cd
Progettomich <progetto.humanitasits@gmail.com>
2026-03-18 17:07:03 +0100

    Update html_report.py

--- report_odl/html_report.py
+++ report_odl/html_report.py
@@ -3,7 +3,7 @@ import pandas as pd
 from datetime import datetime
 from jinja2 import Environment, FileSystemLoader
 
-def build_html_report(tecnico: str, df_t: pd.DataFrame, grafico_b64: str) -> str:
+def build_html_report(tecnico: str, df_t: pd.DataFrame, df_rdi_desc: pd.DataFrame, df_rdi_asc: pd.DataFrame, grafico_odl_b64: str, grafico_rdi_b64: str) -> str:
     """
     Costruisce il report HTML leggendo il template esterno e iniettando i dati.
     """
@@ -34,15 +34,11 @@ def build_html_report(tecnico: str, df_t: pd.DataFrame, grafico_b64: str) -> str
 
     # 4. Inietta i dati nel template (Render)
     html_finale = template.render(
-        nome=tecnico,
-        data_gen=datetime.now().strftime("%d/%m/%Y %H:%M"),
-        # I nomi a sinistra devono combaciare con i {{ NOME }} nel file HTML
-        IN_CORSO=conteggi_stato.get("IN CORSO", 0),
-        SOSPESI=conteggi_stato.get("SOSPESO", 0), # Potrebbe essere "SOSPESI" a seconda di come risponde l'API
-        CHIUSI=conteggi_stato.get("CHIUSO", 0),   # Potrebbe essere "CHIUSI"
-        RDI_NON_PRESI=conteggi_stato.get("RDI NON PRESI", 0),
-        grafico_base64=grafico_b64,
-        odl_list=odl_list
+        # ... (le tue altre variabili) ...
+        grafico_base64_odl=grafico_odl_b64,  # <- Il grafico a barre
+        grafico_base64_rdi=grafico_rdi_b64,  # <- Il grafico a torta
+        odl_list=odl_list,
+        rdi_desc_list=rdi_desc_list,
+        rdi_asc_list=rdi_asc_list
     )
-    
     return html_finale
